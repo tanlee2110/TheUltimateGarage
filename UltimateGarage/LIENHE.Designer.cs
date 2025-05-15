9343217c4d5406230ff625c3daa8c9ed6a02ee64
curyhao123 <ccuurryy2005@gmail.com>
2025-05-15 18:15:24 +0700

    khong co gi

---
+++ UltimateGarage/LIENHE.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace UltimateGarage
+{
+    partial class LIENHE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            thoatbtn = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+            label1.Location = new Point(15, 26);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(732, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Nhóm 2 Nhập môn Công nghệ phần mềm - SE104.P23";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold | FontStyle.Italic);
+            label2.Location = new Point(15, 85);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(238, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Đội ngũ lập trình viên:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold | FontStyle.Italic);
+            label3.Location = new Point(15, 125);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(156, 30);
+            label3.TabIndex = 1;
+            label3.Text = "Hoàng Văn Tài";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold | FontStyle.Italic);
+            label4.Location = new Point(15, 154);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(191, 30);
+            label4.TabIndex = 1;
+            label4.Text = "Võ Khôi Bình Minh";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold | FontStyle.Italic);
+            label5.Location = new Point(15, 182);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(146, 30);
+            label5.TabIndex = 1;
+            label5.Text = "Trần Anh Hào";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold | FontStyle.Italic);
+            label6.Location = new Point(15, 211);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(196, 30);
+            label6.TabIndex = 1;
+            label6.Text = "Lê Phước Ngọc Tân";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold | FontStyle.Italic);
+            label7.Location = new Point(15, 240);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(200, 30);
+            label7.TabIndex = 1;
+            label7.Text = "Chang Chien Cheng";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold | FontStyle.Italic);
+            label8.Location = new Point(15, 281);
+            label8.Margin = new Padding(4, 0, 4, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(300, 30);
+            label8.TabIndex = 1;
+            label8.Text = "Email: vippro@gm.uit.edu.vn";
+            // 
+            // thoatbtn
+            // 
+            thoatbtn.Location = new Point(656, 312);
+            thoatbtn.Margin = new Padding(4, 4, 4, 4);
+            thoatbtn.Name = "thoatbtn";
+            thoatbtn.Size = new Size(118, 36);
+            thoatbtn.TabIndex = 2;
+            thoatbtn.Text = "Thoát";
+            thoatbtn.UseVisualStyleBackColor = true;
+            // 
+            // LIENHE
+            // 
+            AcceptButton = thoatbtn;
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.HighlightText;
+            CancelButton = thoatbtn;
+            ClientSize = new Size(789, 364);
+            Controls.Add(thoatbtn);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label8);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Margin = new Padding(4, 4, 4, 4);
+            MaximizeBox = false;
+            Name = "LIENHE";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Liên hệ";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Button thoatbtn;
+    }
+}
