f3730100b8db4ddfd7273788c927e97802f88f71
curyhao123 <ccuurryy2005@gmail.com>
2025-05-16 22:23:07 +0700

    Thongtinpm

--- UltimateGarage/Properties/Resources.Designer.cs
+++ UltimateGarage/Properties/Resources.Designer.cs
@@ -299,5 +299,15 @@ namespace UltimateGarage.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Untitled_removebg_preview {
+            get {
+                object obj = ResourceManager.GetObject("Untitled-removebg-preview", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
