f24036b42aebc8afe19e2dead20d588bbe97cea7
RiceFordinr <truongkienthanh6705@gmail.com>
2025-05-29 08:11:53 +0700

    (feat) Danh Sach Hieu Xe

--- UltimateGarage/THEMHIEUXE.Designer.cs
+++ UltimateGarage/THEMHIEUXE.Designer.cs
@@ -45,9 +45,8 @@
             panel1.BackColor = Color.MidnightBlue;
             panel1.Controls.Add(label1);
             panel1.Location = new Point(-1, -1);
-            panel1.Margin = new Padding(4, 4, 4, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1000, 82);
+            panel1.Size = new Size(800, 66);
             panel1.TabIndex = 0;
             // 
             // label1
@@ -56,44 +55,42 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 19.8F, FontStyle.Bold);
             label1.ForeColor = SystemColors.Control;
-            label1.Location = new Point(169, 12);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(135, 10);
             label1.Name = "label1";
-            label1.Size = new Size(299, 54);
+            label1.Size = new Size(249, 45);
             label1.TabIndex = 2;
             label1.Text = "THÊM HIỆU XE";
             // 
             // thembtn
             // 
             thembtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            thembtn.Location = new Point(419, 131);
-            thembtn.Margin = new Padding(4, 4, 4, 4);
+            thembtn.Location = new Point(335, 105);
             thembtn.Name = "thembtn";
-            thembtn.Size = new Size(118, 36);
+            thembtn.Size = new Size(94, 29);
             thembtn.TabIndex = 62;
             thembtn.Text = "Thêm";
             thembtn.UseVisualStyleBackColor = true;
+            thembtn.Click += thembtn_Click;
             // 
             // thoatbtn
             // 
             thoatbtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            thoatbtn.Location = new Point(544, 131);
-            thoatbtn.Margin = new Padding(4, 4, 4, 4);
+            thoatbtn.Location = new Point(435, 105);
             thoatbtn.Name = "thoatbtn";
-            thoatbtn.Size = new Size(118, 36);
+            thoatbtn.Size = new Size(94, 29);
             thoatbtn.TabIndex = 63;
             thoatbtn.Text = "Thoát";
             thoatbtn.UseVisualStyleBackColor = true;
+            thoatbtn.Click += thoatbtn_Click;
             // 
             // label8
             // 
             label8.Anchor = AnchorStyles.Top;
             label8.AutoSize = true;
             label8.Font = new Font("Segoe UI", 13.8F);
-            label8.Location = new Point(44, 85);
-            label8.Margin = new Padding(4, 0, 4, 0);
+            label8.Location = new Point(35, 68);
             label8.Name = "label8";
-            label8.Size = new Size(117, 38);
+            label8.Size = new Size(95, 31);
             label8.TabIndex = 68;
             label8.Text = "Hiệu xe:";
             // 
@@ -113,23 +110,23 @@
             themtxtbox.Font = new Font("Segoe UI", 9F);
             themtxtbox.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
             themtxtbox.IconRightCursor = Cursors.AppStarting;
-            themtxtbox.Location = new Point(170, 90);
-            themtxtbox.Margin = new Padding(4, 5, 4, 5);
+            themtxtbox.Location = new Point(136, 72);
+            themtxtbox.Margin = new Padding(3, 4, 3, 4);
             themtxtbox.Name = "themtxtbox";
             themtxtbox.PlaceholderText = "";
             themtxtbox.SelectedText = "";
             themtxtbox.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            themtxtbox.Size = new Size(449, 30);
+            themtxtbox.Size = new Size(359, 24);
             themtxtbox.TabIndex = 69;
             // 
             // THEMHIEUXE
             // 
             AcceptButton = thembtn;
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.HighlightText;
             CancelButton = thoatbtn;
-            ClientSize = new Size(676, 182);
+            ClientSize = new Size(541, 146);
             Controls.Add(themtxtbox);
             Controls.Add(label8);
             Controls.Add(thembtn);
@@ -137,7 +134,6 @@
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.Fixed3D;
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(4, 4, 4, 4);
             MaximizeBox = false;
             Name = "THEMHIEUXE";
             StartPosition = FormStartPosition.CenterScreen;
