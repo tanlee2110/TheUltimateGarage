b166af575722e165cfbc0b5f969ba70dd57098c3
RiceFordinr <truongkienthanh6705@gmail.com>
2025-05-31 05:59:43 +0700

    (feat) DS Phieu Nhap Kho VTPT

--- UltimateGarage/DSPNKVTPT.cs
+++ UltimateGarage/DSPNKVTPT.cs
@@ -20,15 +20,84 @@ namespace UltimateGarage
             InitializeComponent();
             HienThi();
         }
-
         public void HienThi()
         {
             phieunhapvtptdtgrid.DataSource = PNKVTPTDAO.Instance.HienThi();
         }
-
-        private void thoatbtn_Click(object sender, EventArgs e)
+        private void thoat_btnClick(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void xemctbtn_Click(object sender, EventArgs e)
+        {
+            CHITIETPNK c = new CHITIETPNK();
+            c.maphieunhap = phieunhapvtptdtgrid.CurrentRow.Cells[0].Value.ToString();
+            c.ngaynhap = phieunhapvtptdtgrid.CurrentRow.Cells[1].Value.ToString();
+            c.ShowDialog();
+        }
+
+        private void xuatbtn_Click(object sender, EventArgs e)
+        {
+            if (phieunhapvtptdtgrid.Rows.Count == 0)
+                MessageBox.Show("Không có thông tin để xuất!");
+            else
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "Excel Workbook|*.xlsx" })
+                {
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        try
+                        {
+                            using (XLWorkbook workbook = new XLWorkbook())
+                            {
+                                workbook.Worksheets.Add(phieunhapvtptdtgrid.DataSource as DataTable, "PNKVTPT");
+
+                                workbook.SaveAs(saveFileDialog.FileName);
+
+
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Xuất file không thành công!");
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        private void timkiembtn_Click(object sender, EventArgs e)
+        {
+            string s = timkiemtxtbox.Text;
+            int ngay = ngaydtpicker.Value.Day;
+            int thang = ngaydtpicker.Value.Month;
+            int nam = ngaydtpicker.Value.Year;
+            if (flag == 2)
+                phieunhapvtptdtgrid.DataSource = PNKVTPTDAO.Instance.TimKiemTheoNgay(ngay, thang, nam);
+            else
+            {
+                if (!string.IsNullOrEmpty(s))
+                {
+                    if (flag == 1)
+                        phieunhapvtptdtgrid.DataSource = PNKVTPTDAO.Instance.TimKiemTheoMa(s);
+
+                }
+                else
+                    HienThi();
+            }
+        }
+
+        private void timngayradio_CheckedChanged(object sender, EventArgs e)
+        {
+            flag = 2;
+        }
+
+        private void timtheomaradio_CheckedChanged_1(object sender, EventArgs e)
+        {
+            flag = 1;
         }
     }
 }
