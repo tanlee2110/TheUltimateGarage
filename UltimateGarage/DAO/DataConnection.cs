658ec6f77340f1833733009191f205483c5d1eef
tlee2110 <lephuocngoctan555@gmail.com>
2025-07-06 14:07:57 +0700

    Update chuc nang xoa

--- UltimateGarage/DAO/DataConnection.cs
+++ UltimateGarage/DAO/DataConnection.cs
@@ -12,10 +12,10 @@ namespace UltimateGarage.DAO
 {
     internal class DataConnection
     {
-        string constr = "Data Source=RYHAT;Initial Catalog=QUANLIGARA;Integrated Security=True";
+        // string constr = "Data Source=vmwin11;Initial Catalog=QUANLIGARA;Integrated Security=True";
         static string machineName = Environment.MachineName;
-        //string constr = String.Concat("Data Source=", machineName,"\\SQLEXPRESS",";Initial Catalog=QUANLIGARA;Integrated Security=True");
-        
+        string constr = String.Concat("Data Source=", machineName, "\\SQLEXPRESS", ";Initial Catalog=QUANLIGARA;Integrated Security=True");
+
         public SqlConnection getConnect()
         {
             Debug.WriteLine(machineName);
